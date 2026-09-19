83b341b5255d27d4b05a7b54fd0c5342bdd744cf
P Ankitha Rani <pankitharani.sirwar@gmail.com>
2026-09-19 10:52:00 +0530

    Add files via upload

---
+++ VariableDefining.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class VariableDefining {
+    public static void main(String[] args){
+        Scanner SC=new Scanner(System.in);
+        String name=SC.next();
+        System.out.println(name);
+
+    }
+}
