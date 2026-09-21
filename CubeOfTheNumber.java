1f62c04ac8b4f076bbb252d1ae9c27b9ba357acb
P Ankitha Rani <pankitharani.sirwar@gmail.com>
2026-09-21 19:04:48 +0530

    Add files via upload

---
+++ CubeOfTheNumber.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class CubeOfTheNumber {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any value:");
+        int n=sc.nextInt();
+        int cube=n*n*n;
+        System.out.println("Cube of the number is:"+cube);
+         
+    }
+}
---
+++ SquareOfTheNumber.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class SquareOfTheNumber {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter any number:");
+        int n=sc.nextInt();
+        int sq=n*n;
+        System.out.println("Square of the number:"+sq);
+    }
+}
